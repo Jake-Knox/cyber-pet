f74ae9e75b2bd52aed2df16209f18b88bf459b71
jake-knox- <jakeknox11@hotmail.co.uk>
2023-02-07 16:42:48 +0000

    changes to index

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const test = null
+
+
+const nameChangeButton = document.getElementById("name_change_button")
+const nameDiv = document.getElementById("name_change")
+const nameChangeInput = document.getElementById("name_change_input")
+const nameChangeSubmit = document.getElementById("name_change_submit")
+
+nameChangeButton.addEventListener("click", () => {
+    nameDiv.style.display = "block"
+})
